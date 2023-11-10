e4a659e2cd0008b6db421f712df172654b2c6a17
Sebastian Zuviria <szuviria@gmail.com>
2023-11-10 20:19:41 -0300

    After

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,35 @@ import classes from './Navbar.module.css'
 import CartWidget from "../CartWidget/CartWidget"
 import { NavLink, useNavigate } from 'react-router-dom'
 
+import { getDocs, collection, query, orderBy } from 'firebase/firestore'
+import { db } from '../../services/firebase/firebaseConfig'
+import { useEffect, useState } from 'react'
+
 const Navbar = () => {
+    const [categories, setCategories] = useState([])
     const navigate = useNavigate()
+
+    useEffect(() => {
+        const categoriesRef = query(collection(db, 'categories'), orderBy('order'))
+        
+        getDocs(categoriesRef)
+            .then(querySnapshot => {
+                const categoriesAdapted = querySnapshot.docs.map(doc => {
+                    const fields = doc.data()
+                    return { id: doc.id, ...fields}
+                })
+
+                setCategories(categoriesAdapted)
+            })
+    }, [])
 
     return (
         <nav className={classes.container}>
             <h1 onClick={() => navigate('/')}>Ecommerce</h1>
             <section>
-                <NavLink to='/category/celular' className={({ isActive }) => isActive ? classes.active : ''}>Celulares</NavLink>
-                <NavLink to='/category/tablet' className={({ isActive }) => isActive ? classes.active : ''}>Tablets</NavLink>
-                <NavLink to='/category/notebook' className={({ isActive }) => isActive ? classes.active : ''}>Notebooks</NavLink>
+                {
+                    categories.map(cat => <NavLink key={cat.id} to={`/category/${cat.slug}`} className={({ isActive }) => isActive ? classes.active : ''}>{cat.name}</NavLink>)
+                }
             </section>
             <CartWidget />
         </nav>
